e0538ff5c22e146bdde1736f78fc99e845f7360a
denis <denis.junuzovic@fleetster.net>
2020-09-27 15:43:40 +0200

    removes static static methods

---
+++ dist/nf-core.js
@@ -0,0 +1,46 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NFCore = void 0;
+const ioc_config_1 = require("./ioc.config");
+const chalk = require("chalk");
+class _NFCore {
+    constructor() {
+        this.routes = new Map();
+        this.prefixes = new Map();
+        this.controllerMiddleware = new Map();
+    }
+    init(app, ...middleware) {
+        this.initAppMiddleware(app, ...middleware);
+        this.bindRoutesToControllers(app);
+    }
+    registerRoute(route) {
+        this.routes.set(route.path, route);
+    }
+    registerPrefix(controllerName, prefix) {
+        this.prefixes.set(controllerName, prefix);
+    }
+    registerControllerMiddleware(controllerName, ...middleware) {
+        this.controllerMiddleware.set(controllerName, [...middleware]);
+    }
+    initAppMiddleware(app, ...middleware) {
+        middleware.forEach((m) => app.use(m));
+    }
+    bindRoutesToControllers(app) {
+        this.routes.forEach((route) => {
+            const controller = ioc_config_1.container.get(route.controllerClass);
+            const controllerClassName = controller.constructor.name;
+            const prefix = this.prefixes.get(controllerClassName);
+            const controllerMiddleware = this.controllerMiddleware.get(controllerClassName);
+            route.path = prefix ? `/${prefix}${route.path}` : route.path;
+            app[route.method](route.path, ...controllerMiddleware, ...route.middleware, controller[route.controllerMethod].bind(controller));
+            if (process.env.NF_LOG_REGISTERED) {
+                const routeName = chalk.green(route.method.toLocaleUpperCase());
+                const routePath = chalk.yellow(route.path);
+                const routeController = chalk.blue(controllerClassName);
+                const routeHandler = chalk.red(route.controllerMethod);
+                console.log(`${routeName} -> ${routePath} -> ${routeController}.${routeHandler}`);
+            }
+        });
+    }
+}
+exports.NFCore = new _NFCore();
--- src/nf-core.ts
+++ src/nf-core.ts
@@ -12,38 +12,38 @@ export interface Route {
     middleware: Function[];
 }
 
-export const NFCore = class {
-    private static routes: Map<string, Route> = new Map<string, Route>();
-    private static prefixes: Map<string, string> = new Map<string, string>();
-    private static controllerMiddleware: Map<string, Function[]> = new Map<string, Function[]>();
+class _NFCore {
+    private routes: Map<string, Route> = new Map<string, Route>();
+    private prefixes: Map<string, string> = new Map<string, string>();
+    private controllerMiddleware: Map<string, Function[]> = new Map<string, Function[]>();
 
-    static init(app: Application, ...middleware: Function[]) {
-        NFCore.initAppMiddleware(app, ...middleware);
-        NFCore.bindRoutesToControllers(app);
+    init(app: Application, ...middleware: Function[]) {
+        this.initAppMiddleware(app, ...middleware);
+        this.bindRoutesToControllers(app);
     }
 
-    static registerRoute(route: Route) {
-        NFCore.routes.set(route.path, route);
+    registerRoute(route: Route) {
+        this.routes.set(route.path, route);
     }
 
-    static registerPrefix(controllerName: string, prefix: string) {
-        NFCore.prefixes.set(controllerName, prefix);
+    registerPrefix(controllerName: string, prefix: string) {
+        this.prefixes.set(controllerName, prefix);
     }
 
-    static registerControllerMiddleware(controllerName: string, ...middleware: Function[]) {
-        NFCore.controllerMiddleware.set(controllerName, [...middleware]);
+    registerControllerMiddleware(controllerName: string, ...middleware: Function[]) {
+        this.controllerMiddleware.set(controllerName, [...middleware]);
     }
 
-    private static initAppMiddleware(app, ...middleware: Function[]) {
+    private initAppMiddleware(app, ...middleware: Function[]) {
         middleware.forEach((m: Function) => app.use(m));
     }
 
-    private static bindRoutesToControllers(app) {
-        NFCore.routes.forEach((route: Route) => {
+    private bindRoutesToControllers(app) {
+        this.routes.forEach((route: Route) => {
             const controller = container.get(route.controllerClass);
             const controllerClassName = controller.constructor.name;
-            const prefix = NFCore.prefixes.get(controllerClassName);
-            const controllerMiddleware = NFCore.controllerMiddleware.get(controllerClassName);
+            const prefix = this.prefixes.get(controllerClassName);
+            const controllerMiddleware = this.controllerMiddleware.get(controllerClassName);
 
             route.path = prefix ? `/${prefix}${route.path}` : route.path;
             app[route.method](
@@ -64,3 +64,5 @@ export const NFCore = class {
         });
     }
 }
+
+export const NFCore = new _NFCore();
