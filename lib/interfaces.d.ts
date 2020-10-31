c448bb0683395182cf5fa6e2c8081b6aae0e3113
denis <denis.junuzovic@fleetster.net>
2020-10-31 14:07:05 +0100

    expose only what is needed

--- lib/interfaces.d.ts
+++ lib/interfaces.d.ts
@@ -18,18 +18,9 @@ export interface Route {
 }
 
 export interface MTSCore {
-    routes: Map<string, Route>;
-    prefixes: Map<string, string>;
-    controllerMiddleware: Map<string, Handler[]>;
-
     init(app: Application): void;
-
     registerRoute(route: Route): void;
-
     registerPrefix(controllerName: string, prefix: string): void;
-
     registerControllerMiddleware(controllerName: string, ...middleware: Handler[]): void;
-
-    asyncErrorHandler(fn): (...args) => Promise<void>;
 }
 
